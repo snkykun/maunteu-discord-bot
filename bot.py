736105354c151e9d9fea8fc5d1cab7520ad008fb
Kyy <68399818+snkykun@users.noreply.github.com>
2021-04-17 19:11:03 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,5 +36,11 @@ async def clips(ctx, *, source=None):
     else:
         vid_link = 'https://www.youtube.com/watch?v=' + str(random.choice(yt_api.clips_vid_ids))
         await ctx.send(vid_link)
+@client.command()
+async def servers(ctx):
+  servers = list(client.guilds)
+  await ctx.send(f"Connected on {str(len(servers))} servers:")
+  await ctx.send('\n'.join(guild.name for guild in guilds))
+
 
 client.run(botkeys.discord_key)
