a1c4bfa0ea68ab5f11acbccf966c76f839282002
Kyy <68399818+snkykun@users.noreply.github.com>
2021-04-17 20:33:18 -0400

    added refresh command

--- bot.py
+++ bot.py
@@ -8,11 +8,13 @@ import emoji
 import asyncio
 import datetime
 from discord.ext import commands
-from discord import Embed, File
+from discord import Embed, File, Member
 
 client = commands.Bot(command_prefix = '$')
 
 ## Setup
+
+# no longer needed as long as working directory is set in service conf
 # os.chdir(os.path.dirname(sys.argv[0]))
 @client.event
 async def on_ready():
@@ -21,6 +23,7 @@ async def on_ready():
     print('Bot is ready with ' + str(len(yt_api.edit_vid_ids)) + ' edit museum videos and ' + str(len(yt_api.clips_vid_ids)) + ' clips in desc videos discovered.')
     custom = discord.Game('Editing Archive')
     await client.change_presence(status=discord.Status.online, activity=custom)
+
 ## commands
 @client.command()
 async def edit(ctx, *, source=None):
@@ -29,6 +32,7 @@ async def edit(ctx, *, source=None):
     else:
         vid_link = 'https://www.youtube.com/watch?v=' + str(random.choice(yt_api.edit_vid_ids))
         await ctx.send(vid_link)
+
 @client.command()
 async def clips(ctx, *, source=None):
     if source == 'source':
@@ -36,11 +40,19 @@ async def clips(ctx, *, source=None):
     else:
         vid_link = 'https://www.youtube.com/watch?v=' + str(random.choice(yt_api.clips_vid_ids))
         await ctx.send(vid_link)
+
 @client.command()
 async def servers(ctx):
-  servers = list(client.guilds)
-  await ctx.send(f"Connected on {str(len(servers))} servers:")
-  await ctx.send('\n'.join(guild.name for guild in servers))
+    servers = list(client.guilds)
+    await ctx.send(f"Connected on {str(len(servers))} servers:")
+    await ctx.send('\n'.join(guild.name for guild in servers))
+
+@client.command()
+@commands.cooldown(1, 43200)
+async def refresh(ctx):
+    yt_api.editplaylistUpdate()
+    yt_api.clipsplaylistUpdate()
+    await ctx.send(f'Refreshed video playlists, {str(len(yt_api.edit_vid_ids))} edit museum videos and {str(len(yt_api.clips_vid_ids))} clips in desc videos discovered.')
 
 
 client.run(botkeys.discord_key)
